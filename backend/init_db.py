4ddf3f6f5288b1b686ddff7cde026919d0784f39
Florencia <flachavez2299@gmail.com>
2025-10-05 18:40:08 -0300

    Backend

--- backend/init_db.py
+++ backend/init_db.py
@@ -4,5 +4,5 @@ from app.database import Base, engine
 from app import models # Al importar models hace que las tablas de models esten "registradas" en Base.metadata. --> Las obtiene de ahi para crearlas 
 
 print("Creando tablas...")
-Base.metadata.create_all(bind=engine)  # Base.metadata = el “catalogo” de todas las tablas definidas en tus modelos.
+Base.metadata.create_all(bind=engine)  
 print("¡Tablas creadas en MySQL!")
